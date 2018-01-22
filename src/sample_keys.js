aeebc83af2da5271d17fe8a71058cc0efb5efa76
kylewill0725 <kylewill0725@outlook.com>
2018-01-22 11:00:51 -0600

    Added gcmKey option to server sample_keys.js

--- src/sample_keys.js
+++ src/sample_keys.js
@@ -2,7 +2,7 @@
     * Created by kylewill0725 on 6/20/2017.
     */
 module.exports = {
-
-    vapidPrivateKey: 'your_vapid_private_key_here',
-    vapidPublicKey: 'your_vapid_public_key_here'
+    vapidPublicKey:"your-public-vapid-key-goes-here",
+    vapidPrivateKey:"your-private-vapid-key-goes-here",
+    gcmKey: "your-gcm-key-goes-here"
 };
